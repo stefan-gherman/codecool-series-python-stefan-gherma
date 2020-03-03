afdb1b504b8790fb9739ea5cdc8d4fe462c656c0
Stefan Gherman <scgheman@gmail.com>
2020-03-03 03:23:10 +0200

    Add event listener for back button.

--- static/scripts/index_page.js
+++ static/scripts/index_page.js
@@ -204,7 +204,97 @@ const main = async () => {
 
 })
 
+    prevButton.addEventListener('click', async function(event){
 
+        nextButton.removeAttribute('disabled');
+        let lastMinIndex = currStart;
+         if (currStart - 15 > 0) {
+        currStart -= 15;
+    } else {
+        currStart = 0;
+        prevButton.setAttribute('disabled', '');
+    }
+
+    if (currStop - 15 > 0) {
+            if (currStop === maxIndex ) {
+                console.log('end');
+                currStop = maxIndex-(maxIndex - lastMinIndex);
+            } else {
+                currStop -= 15;
+            }
+        } else {
+            currStop = 0;
+
+        }
+    tableContainer.innerHTML = '';
+    table.innerHTML = '';
+    tableBody.innerHTML='';
+    console.log(table);
+    console.log(currStart, currStop);
+
+    for (let j = currStart; j < currStop; j++) {
+        let tableRow = document.createElement('tr');
+        let tableData = document.createElement('td');
+
+
+        tableData.innerHTML = shows[j]['title'];
+        tableRow.appendChild(tableData);
+
+        tableData = document.createElement('td');
+        tableData.innerHTML = shows[j]['year'];
+        tableRow.appendChild(tableData);
+
+        tableData = document.createElement('td');
+        tableData.innerHTML = shows[j]['run_time'];
+        tableRow.appendChild(tableData);
+
+
+        tableData = document.createElement('td');
+        tableData.innerHTML = shows[j]['rating'];
+        tableRow.appendChild(tableData);
+
+
+        tableData = document.createElement('td');
+        if (shows[j]['genres'].length < 3) {
+            for (let i = 0; i < shows[j]['genres'].length; i++) {
+                if (i !== shows[j]['genres'].length - 1) {
+                    tableData.innerHTML = tableData.innerHTML + shows[j]['genres'][i] + ', ';
+                } else {
+                    tableData.innerHTML = tableData.innerHTML + shows[j]['genres'][i];
+                }
+            }
+        } else {
+            for (let i = 0; i < 3; i++) {
+                if (i !== 2) {
+                    tableData.innerHTML = tableData.innerHTML + shows[j]['genres'][i] + ', ';
+                } else {
+                    tableData.innerHTML = tableData.innerHTML + shows[j]['genres'][i];
+                }
+            }
+        }
+        tableRow.appendChild(tableData);
+
+
+        tableData = document.createElement('td');
+        let tableLink = document.createElement('a');
+        let tableBtn = document.createElement('button');
+        tableBtn.innerText = 'See More';
+        tableLink.setAttribute('href', `/show/${shows[j]['show_id']}`);
+        tableLink.appendChild(tableBtn);
+        tableData.appendChild(tableLink);
+        tableRow.appendChild(tableData);
+
+
+        tableRow.appendChild(tableData);
+        tableBody.appendChild(tableRow);
+
+
+    }
+
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+    tableContainer.appendChild(table);
+    })
 };
 
 
