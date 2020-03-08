23ddb5d55dc948c2eb1a36a86c5ee0a3ad43e9b7
Stefan Gherman <scgheman@gmail.com>
2020-03-09 00:25:15 +0200

    Add query to return show list ordered after a parameter.

--- data/queries.py
+++ data/queries.py
@@ -3,8 +3,11 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select(
-        "SELECT shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, round(shows.rating,2) as rtg , round(avg(shows.runtime),2) as run_time FROM shows left JOIN show_genres on shows.id = show_genres.show_id left JOIN genres on show_genres.genre_id = genres.id GROUP BY shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, shows.rating order by shows.rating DESC;")
+        "SELECT shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, round(shows.rating,2) as rtg , round(avg(shows.runtime),2) as run_time FROM shows left JOIN show_genres on shows.id = show_genres.show_id left JOIN genres on show_genres.genre_id = genres.id GROUP BY shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, shows.rating order by shows.id;")
 
 
-def get_shows_with_param(param, order):
-    pass
+def get_shows_with_param(param, ordering):
+    return data_manager.execute_select(
+        f"SELECT shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, round(shows.rating,2) as rtg , round(avg(shows.runtime),2) as run_time FROM shows  left JOIN show_genres on shows.id = show_genres.show_id left JOIN genres on show_genres.genre_id = genres.id GROUP BY shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, shows.rating  order by {param} {ordering};")
+
+
