f9663bdbd0e62870527ee5eb0f9cfcd5689c2258
Stefan Gherman <scgheman@gmail.com>
2020-03-09 20:51:48 +0200

    Add query to get show by id.

--- data/queries.py
+++ data/queries.py
@@ -11,3 +11,12 @@ def get_shows_with_param(param, ordering):
         f"SELECT shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, round(shows.rating,2) as rtg , round(avg(shows.runtime),2) as run_time FROM shows  left JOIN show_genres on shows.id = show_genres.show_id left JOIN genres on show_genres.genre_id = genres.id GROUP BY shows.title, shows.id, show_genres.show_id, show_genres.genre_id, genres.name, shows.year, shows.rating  order by {param} {ordering};")
 
 
+def get_show_from_id(show_id):
+    return data_manager.execute_select(
+        f" SELECT shows.title, shows.id, shows.year, shows.overview, shows.trailer, array_agg(distinct  genres.name) as genres "
+        f" FROM shows "
+        f" LEFT join show_genres ON shows.id = show_genres.show_id"
+        f" LEFT JOIN genres ON show_genres.genre_id = genres.id"
+        f" where shows.id = {show_id}"
+        f" GROUP BY shows.title, shows.id, shows.year, shows.overview, shows.trailer;"
+    )
